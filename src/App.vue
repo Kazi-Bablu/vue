4bfe71bb42761591dd254e09ceefe42acbc9768a
kazi bablu <bablu@shuttlebd.com>
2021-04-19 03:03:30 +0600

    vue js component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Home data="Home Data" msg="Hi From message"></Home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Home from "./components/Home";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello From Home Component</h1>
+    <h2>{{ data }}</h2>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    data: String,
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
